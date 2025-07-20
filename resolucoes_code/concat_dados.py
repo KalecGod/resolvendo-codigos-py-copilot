f626b3500416904d6beeb78bcdaef8c07d81337c
Adalberto Carlos de Oliveira Júnior <137123552+KalecGod@users.noreply.github.com>
2025-07-20 03:18:49 +0000

    add recente

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,20 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+
+
+print("Vamos receber dois dados diferentes do usuário e concatená-los em uma única string!")
+
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ") 
+
+nomeSobrenome = nome + " " + sobrenome 
+
+print("O nome completo é:", nome+sobrenome)
+print("O nome completo é:", nome + " " + sobrenome)
+print("O nome completo é:", f"{nome} {sobrenome}")
+print("O nome completo é:", "{} {}".format(nome, sobrenome))
+print("O nome completo é:", " ".join([nome, sobrenome]))        
+
+
+
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,10 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+
+print("Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado.")  
+
+texto = input ("Digite um texto: ")
+numero = int(input("Digite um número inteiro: "))   
+
+for i in range(numero):
+    print(texto)    
